03da477647af71b31c386977740d3a2acd8487de
lifeifei <1032188134@qq.com>
2018-11-26 11:10:27 +0800

    增加md说明

--- juc/src/main/java/com/carty/juc/keyword/testVolatile/TestVolatile.java
+++ juc/src/main/java/com/carty/juc/keyword/testVolatile/TestVolatile.java
@@ -12,6 +12,20 @@ package com.carty.juc.keyword.testVolatile;
  */
 public class TestVolatile {
 
+    /**
+     * 结果解释：
+     * 程序开始运行过后有两个线程在执行，一个是主线程Main，一个是子线程，
+     * JDK会默认为每一个线程都提供一个缓存，提升效率，
+     * 这就导致了一个问题，两个线程都拥有一个缓存的flag值，
+     * 子线程虽然执行了flag = true，但此时修改的flag值只是子线程自己副本的flag值，Main也是读取自己的flag值
+     *
+     * 所以没有被volatile修饰的demo不会执行指定的输出
+     *
+     * @param args
+     * @return void
+     * @author Lifeifei
+     * @date 2018/11/26 10:27
+     */
     public static void main(String[] args) {
 //        test01();
         test02();
